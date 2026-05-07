8af26c84f5ecaab090cf20f3e648be2ca1e54d81
fakihashakil <fakiha.shakil98@gmail.com>
2026-05-07 15:05:00 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Software Security Exam" << endl;
+    cout << "Name: Syed Aadil Ahmed" << endl;
+    cout << "Student ID: 4339" << endl;
+    return 0;
+}
